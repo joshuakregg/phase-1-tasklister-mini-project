9bfa0b19f67b23a3daeaeee797b8936d1fb1f57f
joshau kregg <kreggjoshua@gmail.com>
2023-03-23 15:18:45 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.getElementById("create-task-form")
+      
+  form.addEventListener('submit', (e) => {
+    
+    e.preventDefault()
+
+   handleToDo(e.target.new_task_description.value)
+
+   form.reset()
+  })
+
+})
+
+function handleToDo(todo) {
+   
+
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'X'
+    li.textContent = todo
+    li.appendChild(btn)
+    document.getElementById("tasks").appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
